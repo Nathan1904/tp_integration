3e79bdd56282f56ebac99002dbffa6088bf2b237
Nathan1904 <nathangabayayra@gmail.com>
2025-06-28 20:59:00 +0200

    Jenkinsfile

--- src/main/java/org/example/Calculateur.java
+++ src/main/java/org/example/Calculateur.java
@@ -1,7 +1,25 @@
 package org.example;
 
 public class Calculateur {
-        public int add(int a, int b) {
-            return a + b;
-        }
+
+    public int add(int a, int b) {
+        return a + b;
     }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division par zéro");
+        return a / b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
--- src/test/java/CalculateurTest.java
+++ src/test/java/CalculateurTest.java
@@ -9,4 +9,40 @@ public class CalculateurTest {
         Calculateur calc = new Calculateur();
         assertEquals(4, calc.add(2, 2));
     }
+
+    @Test
+    void soustractionFonctionne() {
+        Calculateur calc = new Calculateur();
+        assertEquals(1, calc.substract(5, 4));
+    }
+
+    @Test
+    void multiplicationFonctionne() {
+        Calculateur calc = new Calculateur();
+        assertEquals(15, calc.multiply(3, 5));
+    }
+
+    @Test
+    void divisionFonctionne() {
+        Calculateur calc = new Calculateur();
+        assertEquals(2, calc.divide(10, 5));
+    }
+
+    @Test
+    void divisionParZeroLanceException() {
+        Calculateur calc = new Calculateur();
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(5, 0));
+    }
+
+    @Test
+    void testIsEvenVrai() {
+        Calculateur calc = new Calculateur();
+        assertTrue(calc.isEven(4));
+    }
+
+    @Test
+    void testIsEvenFaux() {
+        Calculateur calc = new Calculateur();
+        assertFalse(calc.isEven(3));
+    }
 }
